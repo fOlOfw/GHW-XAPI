9099e7616258a61de190f94ecd80bbdec356c461
Filiz/feliziosso <fOlOfw@users.noreply.github.com>
2024-04-17 01:01:00 +0000

    scraped all of the book titles out of all the available pages

--- web_scraping/web_scrape.py
+++ web_scraping/web_scrape.py
@@ -8,8 +8,9 @@ book_titles = []
 cookies = {
     'ccsid': '784-5723893-5304937',
     'locale': 'en',
-    '_session_id2': 'ca09aa7361979d45e1427ef5e5cb2761',
     'blocking_sign_in_interstitial': 'true',
+    'csm-sid': '383-3833589-7576837',
+    '_session_id2': 'a46d58853fa45a605c536c80c798f677',
 }
 
 headers = {
@@ -18,26 +19,60 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.5',
     # 'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
-    'Referer': 'https://www.goodreads.com/genres/science',
-    # 'Cookie': 'ccsid=784-5723893-5304937; locale=en; _session_id2=ca09aa7361979d45e1427ef5e5cb2761; blocking_sign_in_interstitial=true',
+    'Referer': 'https://www.goodreads.com/list/show/8231.Best_Books_About_Mathematics',
+    # 'Cookie': 'ccsid=784-5723893-5304937; locale=en; blocking_sign_in_interstitial=true; csm-sid=383-3833589-7576837; _session_id2=a46d58853fa45a605c536c80c798f677',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-User': '?1',
+    'If-None-Match': 'W/"d0429aa9fcad25959398d23816ac0233"',
 }
 
-response = requests.get(
-    'https://www.goodreads.com/list/show/8231.Best_Books_About_Mathematics',
-    headers=headers
-)
-pattern = r' <a title="([^"]*)'
-matches = re.findall(pattern, response.text)
-#print(matches) 
+for i in range(1, 10):
+    params = {
+        'page': i,
+    }
+    print("Scraping page", params)
 
-with open('book_titles.txt','w') as f: #creates a text file and stores the data
-    for match in matches:
-        f.write(match.strip() + '\n')
-        #book_titles.append(match.strip())
-        #print(match.strip())
+    response = requests.get('https://www.goodreads.com/list/show/8231.Best_Books_About_Mathematics?page={i}', params = params, headers = headers)
+    #response = requests.get('https://www.goodreads.com/list/show/8231.Best_Books_About_Mathematics',headers=headers)
+    pattern = r' <a title="([^"]*)'
+    matches = re.findall(pattern, response.text)
+    #print(matches) 
+
+    #THIS BELOW COMMENTED PART WAS THE FIRST TRY - Tested with "next_page disabled" but didn't work
+    # # if "next_page disabled" in response.text:
+    # if "current" != "href=" " " in response.text:
+    #     print("end of the book title pages", params)
+    #     break
+    # else:
+    #     for match in matches:
+    #         book_titles.append(match.strip())
+
+    current_tag = re.search(r'<em class="current">([^<]*)</em>', response.text)
+    if current_tag:
+        current_content = current_tag.group(1).strip()  # Get the content inside the <em> tag and remove leading/trailing whitespace
+        for match in matches:
+            if current_content:
+            # If there is content inside the <em> tag
+                book_titles.append(match.strip())
+            else:
+            # If the <em> tag is empty
+                print("End of the book title pages", params)
+                break
+    else:
+        print("No 'current' class found!")
+        break  
+        
+with open('book_titles_collection.txt','w') as f: #creates a text file and stores the data
+    for book_title in book_titles:
+       f.write(book_title.strip() + '\n')
+         
+
+# with open('book_titles.txt','w') as f: #creates a text file and stores the data
+#    for match in matches:
+#        f.write(match.strip() + '\n')
+            # book_titles.append(match.strip())
+            # print(match.strip())
 
